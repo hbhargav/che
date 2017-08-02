4b9dd9b13d09b47fcabdf806ec08dd6ccd0492ee
Sergii Kabashniuk <skabashniuk@redhat.com>
2017-08-02 15:23:51 +0300

    Used Google Java Codestyle

--- plugins/plugin-testing-java/plugin-testing-testng/che-plugin-testing-testng-ide/src/main/java/org/eclipse/che/plugin/testing/testng/ide/inject/TestNgGinModule.java
+++ plugins/plugin-testing-java/plugin-testing-testng/che-plugin-testing-testng-ide/src/main/java/org/eclipse/che/plugin/testing/testng/ide/inject/TestNgGinModule.java
@@ -1,4 +1,4 @@
-/*******************************************************************************
+/*
  * Copyright (c) 2012-2017 Codenvy, S.A.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -7,23 +7,20 @@
  *
  * Contributors:
  *   Codenvy, S.A. - initial API and implementation
- *******************************************************************************/
+ */
 package org.eclipse.che.plugin.testing.testng.ide.inject;
 
 import com.google.gwt.inject.client.AbstractGinModule;
 import com.google.gwt.inject.client.multibindings.GinMultibinder;
-
 import org.eclipse.che.ide.api.extension.ExtensionGinModule;
 import org.eclipse.che.plugin.testing.ide.TestAction;
 import org.eclipse.che.plugin.testing.testng.ide.TestNgTestAction;
 
-/**
- * Gin module for TestNg extension.
- */
+/** Gin module for TestNg extension. */
 @ExtensionGinModule
 public class TestNgGinModule extends AbstractGinModule {
-    @Override
-    protected void configure() {
-        GinMultibinder.newSetBinder(binder(), TestAction.class).addBinding().to(TestNgTestAction.class);
-    }
+  @Override
+  protected void configure() {
+    GinMultibinder.newSetBinder(binder(), TestAction.class).addBinding().to(TestNgTestAction.class);
+  }
 }
